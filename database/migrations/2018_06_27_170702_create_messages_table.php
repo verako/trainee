9051d3a67a62b03b43c4d6c470e42b2dbe9cb616
vera.ko <vera.kopylchuk@gmail.com>
2018-06-27 20:40:29 +0300

    create table messages

---
+++ database/migrations/2018_06_27_170702_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('teme');
+            $table->string('message');
+            $table->string('filename',255);
+            $table->string('filepath',255);
+            $table->string('filemime',255);
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
